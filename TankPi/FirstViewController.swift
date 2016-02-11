2bdc816afe7c388f2c9c07493bf9f65dbeeace04
Kevin McGill <kevin@mcgilldevtech.com>
2016-02-10 21:21:27 -0600

    Just doing a web view.

--- TankPi/FirstViewController.swift
+++ TankPi/FirstViewController.swift
@@ -8,23 +8,17 @@
 
 import UIKit
 
-class FirstViewController: BaseViewController, HTTPsterDelegate {
+class FirstViewController: BaseViewController {
     
     
-    let imageUrl = NSURL(string: "https://tankpi.mcgilln.com/tank.jpg")
-    let tempUrl = NSURL(string: "https://tankpi.mcgilln.com/temp.php")
-
+    let url = NSURL(string: "https://tankpi.mcgilln.com/")
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var webview: UIWebView!
     @IBOutlet weak var refresh: UIButton!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Make delegate
-        HTTPster.shared.delegate = self
-        
         doRefresh()
     }
 
@@ -35,36 +29,8 @@ class FirstViewController: BaseViewController, HTTPsterDelegate {
     }
     
     func doRefresh() {
-        //Get things
-        HTTPster.shared.makeRequest(imageView, httpType: HTTPster.HTTPMethod.GET, contentType: nil, url: imageUrl!, headers: nil, postBody: nil)
-        HTTPster.shared.makeRequest("temp", httpType: HTTPster.HTTPMethod.GET, contentType: nil, url: tempUrl!, headers: nil, postBody: nil)
+        webview.loadRequest(NSURLRequest(URL: url!))
     }
     
-    func didRetrieveResponse(tag: Any, response: NSURLResponse?, responsedata: NSData?, error: NSError?) {
-        
-        if let image = tag as? UIImageView {
-            if image == imageView {
-                if let goodData = responsedata {
-                    print("good image")
-                    imageView.image = UIImage(data: goodData)
-                }
-            }
-        }
-        
-        if let goodString = tag as? String {
-            if goodString == "temp" {
-                //Its the temp.
-                if let goodData = responsedata {
-                    let tempResponse = NSString(data: goodData, encoding: NSUTF8StringEncoding)
-                    if let goodResponse = tempResponse as? String {
-                        let tempIs = goodResponse.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) + " F"
-                        print(tempIs)
-                        label.text = tempIs
-                    }
-                    
-                }
-            }
-        }
-    }
 }
 
