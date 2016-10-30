5c214eb452dce6669fd64e0c17b04c02d8709e88
Kevin McGill <kevin@mcgilldevtech.com>
2016-10-30 12:51:59 -0500

    Remove default index file. Update README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TankPy
+
+## Repo has 3 Projects
+- TankPi - iOS App to turn Tank lights on and off.
+- ApachePi - PHP Scripts for web interface.
+- TankPy - Python Scripts to control RaspberyPi.
--- TankPi/HTTPster.swift
+++ TankPi/HTTPster.swift
@@ -1,6 +1,5 @@
 //
 //  HTTPster.swift
-//  CheeseBall
 //
 //  Created by Kevin on 11/1/14.
 //  Copyright (c) 2014 Kevin McGill d/b/a McGill DevTech. All rights reserved.
@@ -17,19 +16,19 @@ public protocol HTTPsterDelegate {
 
 
 public class HTTPster : NSObject {
-    
+
     public class var shared : HTTPster {
         struct Static {
             static let instance : HTTPster = HTTPster()
         }
         return Static.instance
     }
-    
+
     public enum HTTPMethod:String {
         case POST = "POST"
         case GET = "GET"
     }
-    
+
     public enum HTTPContentType:String {
         case URL_ENCODED = "application/x-www-form-urlencoded"
         case TEXT_HTML = "text/html"
@@ -40,19 +39,19 @@ public class HTTPster : NSObject {
     public var delegate:HTTPsterDelegate?
     let HTTP_TIMEOUT = 15.0
 
-    
+
     public func makeRequest(tag:Any, httpType:HTTPMethod, contentType:HTTPContentType?, url:NSURL, headers:[String:String]?, postBody: String?) {
-        
+
         let request:NSMutableURLRequest = NSMutableURLRequest()
         request.URL = url
         request.HTTPMethod = httpType.rawValue
         request.timeoutInterval = HTTP_TIMEOUT
-        
+
         if let type = contentType {
             request.setValue(type.rawValue, forHTTPHeaderField: "Content-Type")
         }
-        
-        
+
+
         if HTTPMethod.POST == httpType {
             if var goodBody = postBody {
                 goodBody = goodBody.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
@@ -67,24 +66,24 @@ public class HTTPster : NSObject {
             for (key, value) in theHeaders {
                 request.setValue(value, forHTTPHeaderField: key)
             }
-            
+
         }
-        
+
         doCall(tag, request: request)
     }
-    
-    
+
+
     func doCall(tag:Any,request:NSMutableURLRequest) {
-        
+
         let session = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) in
 
             dispatch_async(dispatch_get_main_queue()){
                     self.delegate?.didRetrieveResponse(tag, response: response, responsedata: data, error: error)
             }
-            
+
         })
         session.resume()
-        
+
     }
 
 }
